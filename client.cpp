77cc916beb8be2bc65658ff7964b022bf734db38
Jormungandr <chistiakov.chist@gmail.com>
2021-07-04 20:21:16 +0300

    Add source files

---
+++ client.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello from client" << std::endl;
+  return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello from server" << std::endl;
+  return 0;
+}
